c6c946f2f7151d9f6d6c552fb60437c4c18ac967
Andrew Cho and Jay Whang <cho-andrew@hotmail.com>
2016-04-25 10:33:19 -0700

    add functionality to show responses of allocated users

---
+++ src/StackOverflow/Controllers/StackOverflowController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc;
+using StackOverflow.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace StackOverflow.Controllers
+{
+    [Authorize]
+    public class StackOverflowController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public StackOverflowController(
+            UserManager<ApplicationUser> userManager,
+            ApplicationDbContext db
+        )
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+            return View(_db.Responses.Where(x => x.User.Id == currentUser.Id));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Response response)
+        {
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+            response.User = currentUser;
+            _db.Responses.Add(response);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
